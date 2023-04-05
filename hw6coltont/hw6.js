a4aeeeb1aa8a12aacd81f756d6ad7b5843f5755a
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2023-04-05 19:54:48 -0400

    working!!

--- hw6coltont/hw6.js
+++ hw6coltont/hw6.js
@@ -6,19 +6,18 @@
 let myShader;
 
 function preload() {
-  // load each shader file (don't worry, we will come back to these!)
   myShader = loadShader('shader.vert', 'shader.frag');
 }
 
 function setup() {
-  // the canvas has to be created with WEBGL mode
   createCanvas(400, 400, WEBGL);
-  describe('a simple shader example that outputs the color red')
+  t = 0
 }
 
 function draw() {
-  // shader() sets the active shader, which will be applied to what is drawn next
-  shader(myShader);
-  // apply the shader to a rectangle taking up the full canvas
-  rect(0,0,width,height);
+  shader(myShader)
+  myShader.setUniform("u_time",t)
+  myShader.setUniform("u_resolution",[width, height])
+  rect(0,0,width,height)
+  t++
 }
--- hw6coltont/shader.vert
+++ hw6coltont/shader.vert
@@ -1,6 +1,17 @@
+// (thank you to Adam Ferriss for the foundation of these example shaders)
+// position information that is used with gl_Position
 attribute vec3 aPosition;
 
+// texture coordinates
+attribute vec2 aTexCoord;
+
+// the varying variable will pass the texture coordinate to our fragment shader
+varying vec2 fragCoord;
+
 void main() {
+  // assign attribute to varying, so it can be used in the fragment
+  fragCoord = aTexCoord;
+
   vec4 positionVec4 = vec4(aPosition, 1.0);
   positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
   gl_Position = positionVec4;
