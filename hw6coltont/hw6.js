aeafb580f0e6110e14ebc8914d80f70532f6d8ab
ColtonTshudy <coltont@vt.edu>
2023-04-05 12:53:04 -0400

    basic shader

--- hw6coltont/hw6.js
+++ hw6coltont/hw6.js
@@ -3,3 +3,22 @@
  * version: 04/05/2023
  */
 
+let myShader;
+
+function preload() {
+  // load each shader file (don't worry, we will come back to these!)
+  myShader = loadShader('shader.vert', 'shader.frag');
+}
+
+function setup() {
+  // the canvas has to be created with WEBGL mode
+  createCanvas(400, 400, WEBGL);
+  describe('a simple shader example that outputs the color red')
+}
+
+function draw() {
+  // shader() sets the active shader, which will be applied to what is drawn next
+  shader(myShader);
+  // apply the shader to a rectangle taking up the full canvas
+  rect(0,0,width,height);
+}
